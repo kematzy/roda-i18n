0783fe240bb2572799833b635914bcb5959683f2
Kematzy <kematzy@gmail.com>
2015-09-13 23:20:13 +0800

    Converted to minitest/spec and added test helpers
    Code extracted and inspired from forme gem by Jeremy Evans.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,28 +1,28 @@
-require 'rubygems'
-require 'rack/test'
-require 'roda'
-require'r18n-core'
+# $:.unshift(File.join(File.dirname(File.dirname(File.expand_path(__FILE__))), 'lib'))
+
 
 ENV['RACK_ENV'] = 'test'
 
-# db_name = DB.get{current_database{}}
-# raise "Doesn't look like a test database (#{db_name}), not running tests" unless db_name =~ /test\z/
+require 'rubygems'
+require 'roda'
+require 'tilt/erubis'
+require 'rack/test'
+require 'r18n-core'
+require 'minitest/autorun'
+require 'minitest/hooks/default'
+require 'kramdown'
+require 'date'
 
-# require 'rspec_sequel_matchers'
-# require 'rspec/collection_matchers'
-require 'rspec-html-matchers'
+require "#{File.dirname(__FILE__)}/../lib/roda/plugins/i18n"
 
-RSpec.configure do |config|
-  config.include Rack::Test::Methods
-  # config.include RspecSequel::Matchers
-  config.include RSpecHtmlMatchers
-  config.mock_with :rspec
-  config.expect_with :rspec
-  config.raise_errors_for_deprecations!
-  # ... other config ...  
-end
-
-class RSpec::Core::ExampleGroup
+class Minitest::HooksSpec
+  include Rack::Test::Methods
+  
+  def rt(path,opts={})
+    get path
+    last_response.body
+  end
+  
   def app(type=nil, &block)
     case type
     when :new
@@ -38,7 +38,7 @@ class RSpec::Core::ExampleGroup
       @app ||= _app{route(&block)}
     end
   end
-  
+
   def req(path='/', env={})
     if path.is_a?(Hash)
       env = path
@@ -49,7 +49,7 @@ class RSpec::Core::ExampleGroup
     env = {"REQUEST_METHOD" => "GET", "PATH_INFO" => "/", "SCRIPT_NAME" => ""}.merge(env)
     @app.call(env)
   end
-  
+
   def status(path='/', env={})
     req(path, env)[0]
   end
@@ -67,11 +67,28 @@ class RSpec::Core::ExampleGroup
   end
   
   def _app(&block)
-    c = Class.new(Roda)
-    c.class_eval(&block)
-    c
+     c = Class.new(Roda)
+     c.plugin :render
+     c.plugin(:not_found){raise "path #{request.path_info} not found"}
+     c.use Rack::Session::Cookie, :secret=>'topsecret'
+     c.class_eval do
+       def erb(s, opts={})
+         render(opts.merge(:inline=>s))
+       end
+     end
+     c.class_eval(&block)
+     c
+  end
+
+  # syntactic sugar
+  def _body
+    last_response.body
   end
   
-  def _body; last_response.body; end
-  def _status; last_response.status; end
+  # syntactic sugar
+  def _status
+    last_response.status
+  end
+  
 end
+
