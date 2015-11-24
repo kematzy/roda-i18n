b6d45e3114e22904ee61659dfe6700af5eaed613
Kematzy <kematzy@gmail.com>
2015-11-24 08:19:00 +0800

    Minor Rubocop code cleanup

--- spec/roda_i18n_coverage.rb
+++ spec/roda_i18n_coverage.rb
@@ -3,9 +3,9 @@ require 'simplecov'
 
 def SimpleCov.roda_i18n_coverage(opts = {})
   start do
-    add_filter "/spec/"
-    add_group('Missing'){|src| src.covered_percent < 100}
-    add_group('Covered'){|src| src.covered_percent == 100}
+    add_filter '/spec/'
+    add_group('Missing') { |src| src.covered_percent < 100 }
+    add_group('Covered') { |src| src.covered_percent == 100 }
     yield self if block_given?
   end
 end
