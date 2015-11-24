b256ae600d5abb44eda237e95e11649d6e41797f
Kematzy <kematzy@gmail.com>
2015-11-24 08:18:32 +0800

    Rubocop & bug fixes

--- lib/roda/plugins/i18n.rb
+++ lib/roda/plugins/i18n.rb
@@ -1,7 +1,11 @@
 require 'r18n-core'
 
+# 
 class Roda
+  
+  # 
   module RodaPlugins
+    
     # The i18n plugin allows you to easily add internationalisation (i18n) and
     # localisation support to your Roda app, by adding the following:
     # 
@@ -15,16 +19,16 @@ class Roda
     #    plugin :i18n, :locale => ['de'], :translations => ['absolute/path/2/i18n']
     # 
     # Please note! 
-    #  1) You must set <tt>opts[:root]</tt> in your app if you don't define the <tt>:translations</tt> path.
+    #  1) You must set +opts[:root]+ in your app if you don't define the +:translations+ path.
     #  
-    # 2) When overriding <tt>:translations</tt> the path given must be absolute.
+    # 2) When overriding <tt>:translations</tt> the path given <b>must be absolute</b>.
     # 
     # The path supports 'wildcards', ie: path/**/i18n so you can load translations from multiple
     # combined apps each with their own <tt>i18n</tt> folder with translations.
     # 
     # Note! when loading translations from multiple sources and the same translation key is used 
-    # in both files, the first loaded file takes precedence, ie: <tt>./i18n/en.yml</tt> takes precedence over
-    # <tt>./apps/app1/i18n/en.yml</tt>
+    # in both files, the first loaded file takes precedence, ie: <tt>./i18n/en.yml</tt> takes 
+    # precedence over <tt>./apps/app1/i18n/en.yml</tt>
     # 
     # == USAGE
     # 
@@ -56,7 +60,7 @@ class Roda
     #      l Time.now, :human   #=> "now"
     #      l Time.now, :full    #=> "3rd of January, 2010 18:54"
     #   
-    # Both the <tt>:t</tt> and <tt>:l</tt> methods are available in the route and template (erb) scopes. ie:
+    # Both the +:t+ and +:l+ methods are available in the route and template (erb) scopes. ie:
     # 
     #     route do |r|
     #       r.root do
@@ -84,7 +88,7 @@ class Roda
     # 
     #     route do |r|
     #       
-    #       r.locale do
+    #       r.locale do    # or r.i18n_locale
     #         r.is 'posts' do 
     #           t.posts.header
     #         end
@@ -95,8 +99,7 @@ class Roda
     # 
     # === <tt>:i18n_set_locale_from</tt> RequestMethod
     # 
-    # Obtains the locale from either ENV, HTTP (browser), Params or Session
-    # values
+    # Obtains the locale from either ENV, HTTP (browser), Params or Session values
     # 
     # 
     # Naturally we can allow browsers to override the default locale within routes, like this:
@@ -123,30 +126,51 @@ class Roda
     # 
     module RodaI18n
       
-      def self.load_dependencies(app, opts={})
-        # app.plugin :render
-        # app.plugin :environments
-      end
+      # default options
+      OPTS = {
+        # set the default locale
+        locale:           'en',
+        # set the default fallback locale
+        default_locale:   'en',
+        # set the default translations.
+        translations:     nil
+      }.freeze
       
-      def self.configure(app, opts={})
-        opts = app.opts[:i18n][:orig_opts].merge(opts) if app.opts[:i18n]
-        app.opts[:i18n] = opts.dup
+            
+      def self.configure(app, opts = OPTS)
+        if app.opts[:i18n]
+          opts = app.opts[:i18n][:orig_opts].merge(opts)
+        else
+          opts = OPTS.merge(opts)
+        end
+        
+        app.opts[:i18n]             = opts.dup
         app.opts[:i18n][:orig_opts] = opts
         opts = app.opts[:i18n]
         
-        ::R18n.default_places = opts[:translations] || File.expand_path('i18n', app.opts[:root])
+        # set the translations path to defaults if nil
+        opts[:translations] = File.expand_path('i18n', app.opts[:root]) if opts[:translations].nil?
+        ::R18n.default_places = opts[:translations]
         
-        opts[:default_locale] = opts[:locale].is_a?(Array) ? opts[:locale].first : opts[:locale] || 'en'
+        # default_locale is either 'en' or the set value, so reset :default_locale if 
+        # it is somehow nil or an empty string ' '
+        if opts[:default_locale].nil? || opts[:default_locale] =~ /^\s*$/
+          opts[:default_locale] = 'en'
+        end
         ::R18n::I18n.default = opts[:default_locale]
         
         ::R18n.clear_cache! if ENV['RACK_ENV'] != 'production'
-        i18n   = R18n::I18n.new(opts[:locale], ::R18n.default_places,
-                                  off_filters: :untranslated,
-                                  on_filters:  :untranslated_html)
+        i18n   = R18n::I18n.new(
+          opts[:locale], 
+          ::R18n.default_places,
+          off_filters:  :untranslated,
+          on_filters:   :untranslated_html
+        )
         ::R18n.set(i18n)
       end
       
-      
+      # methods used within Roda's route block
+      # 
       module RequestMethods
         
         # Obtains the locale from either ENV, HTTP (browser), Params or Session
@@ -155,16 +179,33 @@ class Roda
         #   route do |r|
         #     # A): set from URL params ie: GET /posts?locale=de
         #     r.i18n_set_locale_from(:params)
+        # 
+        #       /url?locale=de
+        #       <%= t.one %>    #=> Ein
+        #       /url?locale=es
+        #       <%= t.one %>    #=> Uno
         #     
         #     # B): set from session[:locale] (if present)
         #     r.i18n_set_locale_from(:session)
+        # 
+        #       session[:locale] = 'de'
+        #       <%= t.one %>    #=> Ein
+        #       session[:locale] = 'es'
+        #       <%= t.one %>    #=> Uno
         #     
         #     # C): set from the browser's HTTP request locale
         #     r.i18n_set_locale_from(:http)
+        # 
+        #       HTTP_ACCEPT_LANGUAGE = 'sv-se;q=1,es;q=0.8,en;q=0.6'
+        #       <%= t.one %>    #=> Ett
         #     
         #     # D): set from the server ENV['LANG'] variable
         #     r.i18n_set_locale_from(:ENV)
         #     
+        #       ENV['LANG'] = 'en_US.UTF8'
+        #         <%= t.one %>    #=> One
+        #       ENV['LANG'] = 'es'
+        #         <%= t.one %>    #=> Uno
         #     
         #     r.is 'posts' do 
         #       t.posts.header # use translations
@@ -174,26 +215,30 @@ class Roda
         def i18n_set_locale_from(type)
           case type.to_sym
           when :http
-            _locale = ::R18n::I18n.parse_http(scope.request.env['HTTP_ACCEPT_LANGUAGE'])
+            loc = ::R18n::I18n.parse_http(scope.request.env['HTTP_ACCEPT_LANGUAGE'])
           when :session
-            _locale = session[:locale] if session[:locale]
+            loc = session[:locale] if session[:locale]
           when :params
-            _locale = scope.request.params['locale'] if scope.request.params['locale']
+            loc = scope.request.params['locale'] if scope.request.params['locale']
           when :ENV
-            _locale = ENV['LANG'].split('.').first if ENV['LANG']
+            # ENV['LANG']="en_US.UTF-8"
+            loc = ENV['LANG'].split('.').first if ENV['LANG']
           else
-            _locale = nil
+            loc = nil
           end
           # sanity check: set to default locale if not set above
-          _locale = ::R18n::I18n.default.to_s if _locale.nil?
+          loc = ::R18n::I18n.default.to_s if loc.nil?
           
-          _i18n = ::R18n::I18n.new(_locale, ::R18n.default_places,
-                                    off_filters: :untranslated, 
-                                    on_filters: :untranslated_html)
-          ::R18n.set(_i18n)
+          i18n = ::R18n::I18n.new(
+            loc, 
+            ::R18n.default_places,
+            off_filters:  :untranslated, 
+            on_filters:   :untranslated_html 
+          )
+          ::R18n.set(i18n)
         end
         
-        # Enables setting temporary :locale.
+        # Enables setting temporary :locale blocks within the routing block.
         # 
         #   route do |r|
         #     
@@ -210,11 +255,15 @@ class Roda
         def i18n_set_locale(locale, &blk)
           locale = ::R18n::I18n.default.to_s if locale.nil?
           
-          _i18n = ::R18n::I18n.new(locale, ::R18n.default_places,
-                                    off_filters: :untranslated, 
-                                    on_filters: :untranslated_html)
-          ::R18n.set(_i18n)
-          yield
+          i18n = ::R18n::I18n.new(
+            locale, 
+            ::R18n.default_places, 
+            off_filters:  :untranslated, 
+            on_filters:   :untranslated_html
+          )
+          ::R18n.set(i18n)
+          yield if block_given?
+          # return # NB!! needed to enable routes below to work
         end
         
         # Sets the locale based upon <tt>:locale</tt> prefixed routes
@@ -230,28 +279,31 @@ class Roda
         #     end
         #   end
         #
-        def locale(opts={}, &blk)
+        def locale(opts = {}, &blk)
           on(':locale', opts) do |l|
-            _locale = l || self.class.opts[:locale]
-            session[:locale] = _locale unless session[:locale]
-            ::R18n.set(_locale)
-            yield
+            loc = l || self.class.opts[:locale]
+            session[:locale] = loc unless session[:locale]
+            ::R18n.set(loc)
+            yield if block_given?
+            return # NB!! needed to enable routes below to work
           end
         end
         alias_method :i18n_locale, :locale
       
+      end # /module RequestMethods
       
-      end #/module RequestMethods
       
       module ClassMethods
         
-        # Return the i18n options for this class.
+        # Return the i18n options for this plugin.
         def i18n_opts
           opts[:i18n]
         end
         
-      end #/module ClassMethods
+      end # /module ClassMethods
       
+      
+      # defines method available within the views / routing block
       module InstanceMethods
         include ::R18n::Helpers
         
@@ -269,8 +321,8 @@ class Roda
         
       end
       
-    end #/module RodaI18n
+    end # /module RodaI18n
     
     register_plugin(:i18n, RodaI18n)
-  end #/module RodaPlugins
-end #/class Roda
+  end # /module RodaPlugins
+end # /class Roda
